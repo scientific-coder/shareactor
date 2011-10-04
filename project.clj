7aadd4d073e8d129dbb44d6df3d6d2989c8f607c
scientific-coder <public-code@bernard-hugueney.org>
2011-10-04 12:27:27 +0200

    initial noir project commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject shareactor "0.1.0-SNAPSHOT"
+            :description "share with friends and/or neighbours"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir "1.2.0"]
+                           [noir-cljs "0.1.0-SNAPSHOT"]]
+            :main shareactor.server)
+
