f9b09ae29bfe625b5314fb0caff1b91baa953a23
scientific-coder <public-code@bernard-hugueney.org>
2011-10-04 13:17:59 +0200

    clojureql dependancy

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
             :description "share with friends and/or neighbours"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.0"]
-                           [noir-cljs "0.1.0-SNAPSHOT"]]
+                           [noir-cljs "0.1.0-SNAPSHOT"]
+                           [pinot "0.1.1-SNAPSHOT"]
+                           [clojureql "1.0.2"]]
             :main shareactor.server)
 
